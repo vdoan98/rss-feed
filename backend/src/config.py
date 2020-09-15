15ac9b0a507f7e11e6cb11c7af9f4b5996b3e123
Vy Doan <vdoan98@gmail.com>
2020-09-15 13:51:18 -0500

    Add config file for setting up environment in different machine.

---
+++ backend/src/config.py
@@ -0,0 +1,8 @@
+
+SECRET_KEY = 'emmanualkant'
+DEBUG = True
+CLIENT_ID = '9N4Gb3f6empZr5irxRVOFaS4BIPZUIqG'
+CLIENT_SECRET = 'J-KSi3l9jVoRNp_SWe3dVzx4rctnyA5oV11OfUYKbUlvnG1yr6yc5I9mHA1ML1Ta'
+API_URL = 'https://vdoan98.us.auth0.com',
+ACCESS_TOKEN='https://vdoan98.us.auth0.com/token'
+AUTHORIZE_URL = 'https://vdoan98.us.auth0.com/token'
